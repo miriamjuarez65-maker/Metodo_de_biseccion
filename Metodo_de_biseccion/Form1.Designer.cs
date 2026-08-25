96e9be14de662e305d47ec611cc938a5fbef436e
miri2 <miri2@MIRIAM>
2026-08-25 09:22:32 -0600

    Agregar archivos de proyecto.

---
+++ Metodo_de_biseccion/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Metodo_de_biseccion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtError = new System.Windows.Forms.TextBox();
+            this.lstResultados = new System.Windows.Forms.ListBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblRaiz = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Lucida Fax", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(204, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Límite inferior (a):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Lucida Fax", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(204, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Límite superior (b):";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Lucida Fax", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(204, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Error permitido:";
+            // 
+            // txtA
+            // 
+            this.txtA.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtA.Location = new System.Drawing.Point(363, 39);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 3;
+            this.txtA.TextChanged += new System.EventHandler(this.txtA_TextChanged);
+            // 
+            // txtB
+            // 
+            this.txtB.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtB.Location = new System.Drawing.Point(362, 85);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 4;
+            // 
+            // txtError
+            // 
+            this.txtError.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtError.Location = new System.Drawing.Point(363, 132);
+            this.txtError.Name = "txtError";
+            this.txtError.Size = new System.Drawing.Size(100, 20);
+            this.txtError.TabIndex = 5;
+            this.txtError.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // lstResultados
+            // 
+            this.lstResultados.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lstResultados.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstResultados.FormattingEnabled = true;
+            this.lstResultados.ItemHeight = 15;
+            this.lstResultados.Location = new System.Drawing.Point(211, 219);
+            this.lstResultados.Name = "lstResultados";
+            this.lstResultados.Size = new System.Drawing.Size(374, 154);
+            this.lstResultados.TabIndex = 6;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnCalcular.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(207, 176);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(82, 25);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblRaiz
+            // 
+            this.lblRaiz.AutoSize = true;
+            this.lblRaiz.BackColor = System.Drawing.Color.Transparent;
+            this.lblRaiz.Font = new System.Drawing.Font("Lucida Fax", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRaiz.Location = new System.Drawing.Point(211, 398);
+            this.lblRaiz.Name = "lblRaiz";
+            this.lblRaiz.Size = new System.Drawing.Size(148, 17);
+            this.lblRaiz.TabIndex = 8;
+            this.lblRaiz.Text = "Raíz aproximada:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Metodo_de_biseccion.Properties.Resources._3c1e6253826f6a009b2a02559b800753;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblRaiz);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lstResultados);
+            this.Controls.Add(this.txtError);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Metodo de biseccíon";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtError;
+        private System.Windows.Forms.ListBox lstResultados;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblRaiz;
+    }
+}
+
---
+++ Metodo_de_biseccion/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metodo_de_biseccion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+            double a = double.Parse(txtA.Text);
+            double b = double.Parse(txtB.Text);
+            double error = double.Parse(txtError.Text);
+
+            double fa = Funcion(a);
+            double fb = Funcion(b);
+
+            if (fa * fb > 0)
+            {
+                MessageBox.Show("El intervalo no contiene un cambio de signo.");
+                return;
+            }
+
+            lstResultados.Items.Clear();
+
+            lstResultados.Items.Add(
+                "Iter.      a          b          xr        f(xr)       Error %");
+
+            lstResultados.Items.Add(
+                "----------------------------------------------------------------");
+
+            double xr = 0;
+            double xrAnterior = 0;
+            double errorAproximado = 100;
+            int iteracion = 0;
+
+            while (errorAproximado > error)
+            {
+                iteracion++;
+
+                xr = (a + b) / 2;
+
+                double fxr = Funcion(xr);
+
+                if (iteracion > 1)
+                {
+                    errorAproximado =
+                        Math.Abs((xr - xrAnterior) / xr) * 100;
+                }
+
+                lstResultados.Items.Add(
+                    iteracion.ToString().PadRight(10) +
+                    a.ToString("F6").PadRight(11) +
+                    b.ToString("F6").PadRight(11) +
+                    xr.ToString("F6").PadRight(11) +
+                    fxr.ToString("F6").PadRight(13) +
+                    errorAproximado.ToString("F6") + "%"
+                );
+
+                if (fa * fxr < 0)
+                {
+                    b = xr;
+                    fb = fxr;
+                }
+                else
+                {
+                    a = xr;
+                    fa = fxr;
+                }
+
+                xrAnterior = xr;
+            }
+
+            lblRaiz.Text = "Raíz aproximada: " + xr.ToString("F6");
+        }
+        private double Funcion(double x)
+        {
+            return 5 * Math.Pow(x, 3) - 5 * Math.Pow(x, 2) + 6 * x - 2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
